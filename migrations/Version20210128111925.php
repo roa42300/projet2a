b676736e1907139945d6d9d65be89bf6eb864998
leoo blaison <leo.blaison@etu.univ-st-etienne.fr>
2021-02-03 16:36:05 +0100

    'test2'

---
+++ migrations/Version20210128111925.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210128111925 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE documents (id INT AUTO_INCREMENT NOT NULL, type_id_id INT NOT NULL, chemin VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, actif TINYINT(1) NOT NULL, INDEX IDX_A2B07288714819A0 (type_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE documents ADD CONSTRAINT FK_A2B07288714819A0 FOREIGN KEY (type_id_id) REFERENCES genre (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE documents');
+    }
+}
---
+++ migrations/Version20210129074504.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210129074504 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE acces (id INT AUTO_INCREMENT NOT NULL, utilisateur_id_id INT NOT NULL, INDEX IDX_D0F43B10B981C689 (utilisateur_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE access (id INT AUTO_INCREMENT NOT NULL, utilisateur_id_id INT NOT NULL, autorisation_id_id INT NOT NULL, document_id_id INT NOT NULL, INDEX IDX_6692B54B981C689 (utilisateur_id_id), INDEX IDX_6692B543B0E139B (autorisation_id_id), INDEX IDX_6692B5416E5E825 (document_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE delet_acces (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE delete_acces (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE acces ADD CONSTRAINT FK_D0F43B10B981C689 FOREIGN KEY (utilisateur_id_id) REFERENCES utilisateur (id)');
+        $this->addSql('ALTER TABLE access ADD CONSTRAINT FK_6692B54B981C689 FOREIGN KEY (utilisateur_id_id) REFERENCES utilisateur (id)');
+        $this->addSql('ALTER TABLE access ADD CONSTRAINT FK_6692B543B0E139B FOREIGN KEY (autorisation_id_id) REFERENCES autorisation (id)');
+        $this->addSql('ALTER TABLE access ADD CONSTRAINT FK_6692B5416E5E825 FOREIGN KEY (document_id_id) REFERENCES documents (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE acces');
+        $this->addSql('DROP TABLE access');
+        $this->addSql('DROP TABLE delet_acces');
+        $this->addSql('DROP TABLE delete_acces');
+    }
+}
